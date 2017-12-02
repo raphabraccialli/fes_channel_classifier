2c41b81b6e62a4d5e1e03c4974adf426b628dca5
Raphael <raphael.braccialli@gmail.com>
2017-12-02 20:04:34 -0200

    dicionario de correntes implementado

--- scripts/channelScanner.py
+++ scripts/channelScanner.py
@@ -14,6 +14,11 @@ from tf import transformations
 import time
 
 
+####### TO DO #######
+# - remover do imu.yaml streamming de dados que não esão sendo usados
+# - remover o publish do loop da main(). Está publicando acima da taxa de atualização das imus e distorcendo o gráfico
+
+
 class ChannelScanner:
 	''' Classe scanner de canais '''
 	def __init__(self):
@@ -29,12 +34,7 @@ class ChannelScanner:
 
 	def init_variables(self):
 		''' Iniciação das variaveis da classe '''
-		#self.numOfChannels = 2
-		self.usedChannels = [1, 3, 5, 7]
-		self.scannedChannels = []
-		for c in self.usedChannels:
-			self.scannedChannels.append(c)
-
+		self.scannedChannels = [1, 3, 5, 7]
 		self.stimMsg = Stimulator()
 		self.stimMsg.channel = [1]
 		self.stimMsg.mode = ['single']
@@ -42,6 +42,9 @@ class ChannelScanner:
 		self.channelIndex = 0
 		self.maxAngle = []
 		self.breaker = False
+		self.channelCurrent = {}
+		for c in self.scannedChannels:
+			self.channelCurrent[c] = 0
 
 	def get_angle(self, data):
 		qx,qy,qz,qw = data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w
@@ -85,9 +88,16 @@ class ChannelScanner:
 
 		while not rospy.is_shutdown():
 			if self.channelIndex < len(self.scannedChannels):
-				self.stimulation_routine()
+				if self.channelCurrent[self.scannedChannels[self.channelIndex]] == 0:
+					self.stimulation_routine()
+				else:
+					self.channelIndex += 1
 			else:
 				break
+
+			#else:
+			#	if 'passou por todos canais' | 'corrente acima do limite':
+			#			break
 
 			rate.sleep()
 
@@ -98,117 +108,6 @@ class ChannelScanner:
 
 		print(self.maxAngle)
 
-
-
-####### TO DO #######
-# - remover do imu.yaml streamming de dados que não esão sendo usados
-# - remover o publish do loop da main(). Está publicando acima da taxa de atualização das imus e distorcendo o gráfico
-
-##################################################
-##### Máquina de estados #########################
-##################################################
-
-##################################################
-##### Funções de Callback ########################
-##################################################
-
-# def angle_callback(data):
-# 	global plotAngle
-# 	global pubStim
-# 	global maxAngle
-# 	global channel
-# 	global breaker
-# 	global counter
-# 	global numOfChannels
-#
-# 	if channel <= numOfChannels:
-#
-# 		qx,qy,qz,qw = data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w
-# 		euler = transformations.euler_from_quaternion([qx, qy, qz, qw], axes='syxz')
-# 		angle = euler[0] * (180/pi)
-# 		plotAngle.publish(euler[0] * (180/pi))
-#
-# 		print('Estimulando canal ' + str(channel))
-# 		stimMsg.pulse_current = [8]
-# 		stimMsg.pulse_width = [500]
-# 		stimMsg.channel = [channel]
-# 		pubStim.publish(stimMsg)
-#
-# 		print(counter)
-#
-# 		if counter < 200:
-# 			if angle > maxAngle[channel-1]:
-# 						maxAngle[channel-1] = angle
-# 			counter += 1
-# 		else:
-# 			stimMsg.pulse_current = [0]
-# 			stimMsg.pulse_width = [0]
-# 			pubStim.publish(stimMsg)
-#
-# 			rospy.sleep(5)
-#
-# 			counter = 0
-# 			channel += 1
-# 	else:
-# 		breaker = True
-#
-#
-#
-# ##################################################
-# ##### Iniciação de variáveis globais #############
-# ##################################################
-#
-# numOfChannels = 2
-#
-# stimMsg = Stimulator()
-# stimMsg.channel = [1]
-# stimMsg.mode = ['single']
-#
-# counter = 0
-#
-# channel = 1
-#
-# maxAngle = []
-#
-# breaker = False
-#
-# ##################################################
-# ##### Loop do ROS ################################
-# ##################################################
-#
-# def channelScanner():
-# 	global plotAngle
-# 	global pubStim
-# 	global numOfChannels
-#
-# 	for i in range(0, numOfChannels):
-# 		maxAngle.append(0)
-#
-# 	# inicia nó
-# 	rospy.init_node('channelScanner', anonymous = True)
-#
-# 	#espera terminar calibragem
-# 	rospy.sleep(5)
-#
-# 	# Inscreve nos canais de pulibação de ângulos de cada sensor
-# 	rospy.Subscriber('imu/angle', Imu, callback = angle_callback, queue_size=1)
-#
-# 	# Publica ângulos  para mostrar em gráfico
-# 	plotAngle = rospy.Publisher('angle', Float64, queue_size = 10)
-#
-# 	# Publica valores de eletroestimulação a serem lidos pelo estimulador
-# 	pubStim = rospy.Publisher('stimulator/ccl_update', Stimulator, queue_size=10)
-#
-# 	while not breaker:
-# 		pass
-#
-# 	stimMsg.pulse_current = [0]
-# 	stimMsg.pulse_width = [0]
-# 	pubStim.publish(stimMsg)
-#
-# 	print(maxAngle)
-#
-#
 
 if __name__ == '__main__':
 	channel_scan = ChannelScanner()
